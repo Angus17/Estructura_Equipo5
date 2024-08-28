1f3cf0d1a602f08f37390540cbaa42785b3244ab
Angus19 <diego.alejocnt@uanl.edu.mx>
2024-08-27 22:01:40 -0600

    Actividad casi terminada

--- Actividad_5_Equipo5.cpp
+++ Actividad_5_Equipo5.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <iomanip>
 #include <cstdlib>
 #include <string>
+#include <limits>
 #include <ctime>
 #include <regex>
 #include <chrono>
+#include <new>
 
 #if defined( unix ) && !defined(__ANDROID__)
     #include <stdio_ext.h>
@@ -17,8 +20,7 @@ using namespace chrono;
 struct Cliente
 {
     int numero_movimiento;
-    string nombre;
-    string apellidos;
+    string nombre_apellidos;
     string cuenta;
     string movimiento;
     double monto_movimiento;
@@ -27,7 +29,7 @@ struct Cliente
 };
 
 static void iniciar_movimiento(Cliente *&, long long int &);
-static void apliar_nodos(Cliente *&);
+static void apliar_nodos(Cliente *&, long long int &);
 static void mostrar_movimientos(Cliente *&);
 static void hacer_corte(Cliente *&);
 
@@ -42,48 +44,44 @@ int main()
 {
     int opcion;
     long long int fondo_caja;
-    bool entrada_invalida;
+    bool entrada_invalida, corte = true;
     Cliente *cabeza_lista = nullptr;
 
-    #if defined( _WIN32 ) || defined( _WIN64 )
-
-        setlocale(LC_CTYPE, "es_MX");
-
-    #elif defined( unix ) && !defined(__ANDROID__)
-
-        setlocale(LC_CTYPE, "es_MX.UTF-8");
-
-    #endif
+    setlocale(LC_CTYPE, "es_MX.UTF-8");
 
     do
     {
-        limpiar_pantalla();
-
-        cout << "Con cuánto dinero desea fondear la caja el día de hoy?: $";
-        limpiar_buffer_STDIN();
-        cin >> fondo_caja;
-
-        entrada_invalida = cin.fail();
-
-        if ( entrada_invalida )
+        if ( corte )
         {
-            cin.clear();
-            mostrar_mensaje_error();
-        }
-        else if( fondo_caja < 300000LL )
+            do
             {
-                cout << "La caja debe tener más de $300,000 para iniciar operaciones. . ." << endl;
-                pausar_terminal();
-            }
-        
-    } while ( entrada_invalida );
-    
+                limpiar_pantalla();
 
-    do
-    {
+                cout << "\nCon cuánto dinero desea fondear la caja el día de hoy?: $";
+                limpiar_buffer_STDIN();
+                cin >> fondo_caja;
+
+                entrada_invalida = cin.fail();
+
+                if ( entrada_invalida )
+                {
+                    cin.clear();
+                    mostrar_mensaje_error();
+                }
+                else if( fondo_caja < 300000LL )
+                    {
+                        cout << "La caja debe tener más de $300,000 para iniciar operaciones. . ." << endl;
+                        pausar_terminal();
+                    }
+                
+            } while ( entrada_invalida || fondo_caja < 300000LL );
+
+            corte = false;
+        }
+
         do
         {
-            //limpiar_pantalla();
+            limpiar_pantalla();
 
             cout << "\t\t=================================================================\n" << endl;
             cout << "\t\t\t\tMENÚ DE BANCO\n\n" << endl;
@@ -110,23 +108,18 @@ int main()
             
         } while (entrada_invalida || (opcion < 1 || opcion > 4));
 
-        //limpiar_pantalla();
+        limpiar_pantalla();
 
         switch ( opcion )
         {
             case 1:
 
-                if( fondo_caja <= MONTO_CORTE )
-                {
-                    cout << "La caja cuenta con $" << fondo_caja << " se recomienda hacer corte de caja. . ." << endl;
-                    pausar_terminal();
-                }
-
-                iniciar_movimiento( cabeza_lista, fondo_caja );
+                apliar_nodos(cabeza_lista, fondo_caja);
 
             break;
 
             case 2:
+
                 if ( cabeza_lista != nullptr )
 
                     mostrar_movimientos(cabeza_lista);
@@ -139,24 +132,39 @@ int main()
             break;
 
             case 3:
-                if ( cabeza_lista != nullptr )
 
+                if ( cabeza_lista != nullptr && fondo_caja >= MONTO_CORTE )
+                {
                     hacer_corte(cabeza_lista);
 
-                else
+                    corte = true;
 
-                    cout << "\t\t\tNo hay movimientos bancarios registrados.\n" << endl;
+                    cout << "CORTE DE CAJA REALIZADO EXITOSAMENTE!. . ." << endl;
+                }
+                else if ( cabeza_lista == nullptr )
+
+                        cout << "\t\t\tNo hay movimientos bancarios registrados.\n" << endl;
+
+                    else
+
+                        cout << "\t\t\tLa caja no cuenta con los fondos suficientes para hacer el corte. . .\n" << endl;
 
                 
             break;
 
             case 4:
-                cout << "Saliendo del programa. . . ." << endl;
+
+                cout << "\t\t\tSaliendo del programa. . . .\n" << endl;
                 seconds(5);
+
             break;
         
         }
-        
+
+        if ( opcion != 4 )
+
+            pausar_terminal();        
+
     } while ( opcion != 4 );
     
     return EXIT_SUCCESS;
@@ -164,10 +172,10 @@ int main()
 
 static void iniciar_movimiento(Cliente *&lista_clientes, long long int &fondo)
 {
-    bool numero_invalido, expresion_valida, entrada_invalida;
+    bool numero_valido, expresion_valida, entrada_invalida;
     regex patron_movimiento("(Deposito|Retiro)");
-    regex patron_nombre_apellidos("([a-zA-ZÁ-Ý\u00f1\u00d1]+)? ([a-zA-ZÁ-Ý\u00f1\u00d1]+)");
-    regex patron_cuenta("[0-9]{1,8}");
+    regex patron_nombre_apellidos(R"(^[a-zA-ZÁ-Ýá-ýÑñ]+([ ]?[a-zA-ZÁ-Ýá-ýÑñ]+)*$)");
+    regex patron_cuenta(R"(^\d{1,8}$)");
 
     do
     {
@@ -177,25 +185,25 @@ static void iniciar_movimiento(Cliente *&lista_clientes, long long int &fondo)
         srand(time(nullptr));
         lista_clientes->numero_movimiento = rand();
 
-        numero_invalido = validar_numero_movimiento(lista_clientes, lista_clientes->numero_movimiento);
+        numero_valido = validar_numero_movimiento(lista_clientes, lista_clientes->numero_movimiento);
 
-        if ( numero_invalido )
+        if ( !numero_valido )
         {
-            //limpiar_pantalla();
+            limpiar_pantalla();
             cout << "Espere un momento por favor. . . ." << endl;
             seconds(10);
         }
 
-    } while ( numero_invalido );
+    } while ( !numero_valido );
 
     do
     {
-        //limpiar_pantalla();
-        cout << "Nombre: ";
+        limpiar_pantalla();
+        cout << "Nombre y apellidos: ";
         limpiar_buffer_STDIN();
-        getline(cin, lista_clientes->nombre);
+        getline(cin, lista_clientes->nombre_apellidos);
 
-        expresion_valida = regex_search(lista_clientes->nombre, patron_nombre_apellidos);
+        expresion_valida = regex_search(lista_clientes->nombre_apellidos, patron_nombre_apellidos);
 
         if ( !expresion_valida )
 
@@ -206,23 +214,7 @@ static void iniciar_movimiento(Cliente *&lista_clientes, long long int &fondo)
 
     do
     {
-        //limpiar_pantalla();
-        cout << "Apellidos: ";
-        limpiar_buffer_STDIN();
-        getline(cin, lista_clientes->apellidos);
-
-        expresion_valida = regex_search(lista_clientes->apellidos, patron_nombre_apellidos);
-
-        if ( !expresion_valida )
-
-            mostrar_mensaje_error();
-
-
-    } while (!expresion_valida);
-
-    do
-    {
-        // limpiar_pantalla();
+        limpiar_pantalla();
 
         cout << "Ingrese su número de cuenta: ";
         limpiar_buffer_STDIN();
@@ -238,8 +230,13 @@ static void iniciar_movimiento(Cliente *&lista_clientes, long long int &fondo)
 
     do
     {
-        //limpiar_pantalla();
+        limpiar_pantalla();
 
+        if ( fondo < MONTO_CORTE )
+        {
+            
+        }
+        
         cout << "Qué tipo de movimiento hará en nuestro banco? (Deposito|Retiro): ";
         limpiar_buffer_STDIN();
         getline(cin, lista_clientes->movimiento);
@@ -254,7 +251,9 @@ static void iniciar_movimiento(Cliente *&lista_clientes, long long int &fondo)
 
     do
     {
-        // limpiar_pantalla();
+        limpiar_pantalla();
+
+        cout << "\t\t\tSALDO DISPONIBLE EN LA CAJA: $" << fondo << endl << endl;
 
         cout << "Monto del movimiento: $";
         limpiar_buffer_STDIN();
@@ -266,14 +265,20 @@ static void iniciar_movimiento(Cliente *&lista_clientes, long long int &fondo)
             cin.clear();
             mostrar_mensaje_error();
         }
-        else if ( lista_clientes->monto_movimiento > 50000 && lista_clientes->movimiento == "Retiro")
+        else if ( (lista_clientes->monto_movimiento > 50000.0 || lista_clientes->monto_movimiento == fondo) && lista_clientes->movimiento == "Retiro")
             {
-                //limpiar_pantalla();
-                cout << "No puedes retirar más de los $50,000 permitidos, favor de pasar a gerencia. . .";
+                limpiar_pantalla();
+                cout << "Tu monto no puede ser retirado por políticas del banco, favor de pasar a gerencia . . ." << endl;
                 pausar_terminal();
             }
+            else if ( lista_clientes->movimiento == "Retiro" && lista_clientes->monto_movimiento > fondo )
+                {
+                    limpiar_pantalla();
+                    cout << "La caja cuenta con $" << fondo << ", tu monto no puede ser procesado por fondos insuficientes. . ." << endl;
+                    pausar_terminal();
+                }
 
-    } while(( entrada_invalida || lista_clientes->monto_movimiento <= 0 ) || ( lista_clientes->monto_movimiento > 50000 && lista_clientes->movimiento == "Retiro" ) );
+    } while(( entrada_invalida || lista_clientes->monto_movimiento <= 0 ) || ( (lista_clientes->monto_movimiento > 50000.0 || lista_clientes->monto_movimiento == fondo || lista_clientes->monto_movimiento > fondo ) && lista_clientes->movimiento == "Retiro" ) );
 
     if ( lista_clientes->movimiento == "Deposito" )
 
@@ -282,26 +287,13 @@ static void iniciar_movimiento(Cliente *&lista_clientes, long long int &fondo)
     else
 
         fondo -= lista_clientes->monto_movimiento;
-
-
-    apliar_nodos(lista_clientes);
-
-    //limpiar_pantalla();
-    cout << "OPERACIÓN REALIZADA CON ÉXITO!" << endl;
-    cout << "Regresando al menú. . . ." << endl;
-    seconds(5);
 }
 
-static void apliar_nodos(Cliente *&lista_clientes)
+static void apliar_nodos(Cliente *&lista_clientes, long long int &_fondo)
 {
     Cliente *nuevo_nodo = new Cliente();
 
-    nuevo_nodo->nombre = lista_clientes->nombre;
-    nuevo_nodo->apellidos = lista_clientes->apellidos;
-    nuevo_nodo->cuenta = lista_clientes->cuenta;
-    nuevo_nodo->movimiento = lista_clientes->movimiento;
-    nuevo_nodo->monto_movimiento = lista_clientes->monto_movimiento;
-    nuevo_nodo->numero_movimiento = lista_clientes->numero_movimiento;
+    iniciar_movimiento(nuevo_nodo, _fondo);
 
     nuevo_nodo->siguiente = nullptr;
     
@@ -313,36 +305,47 @@ static void apliar_nodos(Cliente *&lista_clientes)
     {
         Cliente *auxiliar = lista_clientes;
 
-        while (auxiliar->siguiente!= nullptr)
+        while ( auxiliar->siguiente != nullptr )
 
             auxiliar = auxiliar->siguiente;
 
         auxiliar->siguiente = nuevo_nodo;
     }
+
+    limpiar_pantalla();
+    cout << "OPERACIÓN REALIZADA CON ÉXITO!" << endl;
+    cout << "Regresando al menú. . . ." << endl;
+    seconds(5);
 }
 
 static void mostrar_movimientos(Cliente *&lista_clientes)
 {
     Cliente *auxiliar = lista_clientes;
 
-    cout << "\t\t+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=" << endl;
+    cout << "\t\t+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=\n" << endl;
 
     while ( auxiliar != nullptr )
     {
         cout << "\t\t\tMovimiento número: " << auxiliar->numero_movimiento << endl;
-        cout << "\t\t\tNombre y Apellidos: " << auxiliar->nombre << " " << auxiliar->apellidos << endl;
+        cout << "\t\t\tNombre y Apellidos: " << auxiliar->nombre_apellidos << endl;
         cout << "\t\t\tCuenta: " << auxiliar->cuenta << endl;
         cout << "\t\t\tTipo de Movimiento: " << auxiliar->movimiento << endl;
-        cout << "\t\t\tMonto: $" << auxiliar->monto_movimiento << endl;
+        cout << "\t\t\tMonto: $" << fixed << setprecision(2) <<auxiliar->monto_movimiento << endl;
         cout << "\n\t\t+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=" << endl;
+
+        auxiliar = auxiliar->siguiente;
     }
-    
-    
 }
 
 static void hacer_corte(Cliente *&lista_clientes)
 {
-    
+    Cliente *auxiliar = lista_clientes;
+
+    while ( lista_clientes != nullptr )
+    {
+        lista_clientes = lista_clientes->siguiente;
+        delete auxiliar;
+    }
 }
 
 static bool validar_numero_movimiento( Cliente *&lista_clientes, const int &numero )
@@ -370,8 +373,6 @@ static void limpiar_buffer_STDIN()
     #elif defined(unix)
         __fpurge(stdin);
     #endif
-
-
 }
 
 static void limpiar_pantalla()
@@ -381,8 +382,6 @@ static void limpiar_pantalla()
     #elif defined(unix)
         system("clear");
     #endif
-
-
 }
 
 
@@ -396,8 +395,6 @@ static void pausar_terminal()
         cin.get();
         limpiar_pantalla();
     #endif
-
-
 }
 
 static void mostrar_mensaje_error()
